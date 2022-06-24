01f179873cba66461cbbe7844ba542e90078a248
tr15tan01 <t.varamashvili@gmail.com>
2022-06-24 23:45:54 +0400

    changed test file

--- sample.test.js
+++ sample.test.js
@@ -4,26 +4,24 @@ const app = express()
 const request = supertest(app)
 
 
-describe('Sample test', () => {
-    test('should be true === true', async () => {
-        const resp = await request.get('/foo')
-        expect(resp.status).toBe(200)
+// describe('Sample test', () => {
+//     test('should be true === true', async () => {
+//         const resp = await request.get('/foo')
+//         expect(resp.status).toBe(200)
 
-    })
-})
-
-
+//     })
+// })
 
 
 const baseUrl = 'https://jsonplaceholder.typicode.com/';
 
-describe('Todos endpoint', () => {
-    it('should return a 200 status code', async () => {
-        const response = await request.get(baseUrl);
+// describe('Todos endpoint', () => {
+//     it('should return a 200 status code', async () => {
+//         const response = await request.get(baseUrl);
 
-        expect(response.statusCode).toBe(200);
-    });
-})
+//         expect(response.statusCode).toBe(200);
+//     });
+// })
 
 
 describe('just simple fummy', () => {
@@ -32,16 +30,16 @@ describe('just simple fummy', () => {
     })
 })
 
-describe('jendpoint test', () => {
-    it('should be 200', async () => {
+// describe('jendpoint test', () => {
+//     it('should be 200', async () => {
 
-        const response = await request.get('/foo')
-        console.log(response.statusCode, 'status code')
-        // .expect('Content-Type', "text/html")
-        // .expect('Content-Length', '15')
-        expect(response.statusCode).toBe(200)
-    })
-})
+//         const response = await request.get('/foo')
+//         console.log(response.statusCode, 'status code')
+//         // .expect('Content-Type', "text/html")
+//         // .expect('Content-Length', '15')
+//         expect(response.statusCode).toBe(200)
+//     })
+// })
 
 // describe('foo endpoint test', () => {
 //     it('foo must be 200', () => {
